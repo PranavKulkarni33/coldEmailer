d3e4eefa5bdb429e507c78861015af8ea17813c0
Pranav Kulkarni <121322862+PranavKulkarni33@users.noreply.github.com>
2024-11-09 15:26:18 -0500

    Updated Auth Service

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -2,44 +2,49 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor(private fireauth: AngularFireAuth, private router : Router) { }
+  
+  constructor(private fireauth: AngularFireAuth, private router: Router) {}
 
   login(username: string, password: string) {
-      this.fireauth.signInWithEmailAndPassword(username, password).then(() => {
+    this.fireauth.signInWithEmailAndPassword(username, password).then(() => {
       localStorage.setItem('token', 'true');
       this.router.navigate(['dashboard']);
-      
     }, err => {
       alert(err.message);
       this.router.navigate(['/login']);
     });
   }
 
-  //signup method
-  signup( username : string, password : string){
-    this.fireauth.createUserWithEmailAndPassword(username, password).then( () => {
-      alert(' SignUp sucessful');  
+  // signup method
+  signup(username: string, password: string) {
+    this.fireauth.createUserWithEmailAndPassword(username, password).then(() => {
+      alert('SignUp successful');
       this.router.navigate(['/login']);
     }, err => {
       alert(err.message);
       this.router.navigate(['/signup']);
     });
-    
   }
 
-  //signout method
-  logout(){
-    this.fireauth.signOut().then( () => {
-        localStorage.removeItem('token');
-        this.router.navigate(['/login']);
+  // signout method
+  logout() {
+    this.fireauth.signOut().then(() => {
+      localStorage.removeItem('token');
+      this.router.navigate(['/login']);
     }, err => {
-        alert(err.message);
-    })
+      alert(err.message);
+    });
+  }
+
+  // Method to get the current user
+  getCurrentUser(): Observable<firebase.User | null> {
+    return this.fireauth.authState;
   }
 }
